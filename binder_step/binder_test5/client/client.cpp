00389c1671defbadccbd9df8af5b4ee8df45297a
paldad <paldad111@gmail.com>
2015-04-22 09:14:04 +0900

    BpBinder for service handle

--- binder_step/binder_test5/client/client.cpp
+++ binder_step/binder_test5/client/client.cpp
@@ -2,7 +2,7 @@
 // client.cpp
 #include "Android.h"
 
-// 1. 드라이버의 정보를 보존하기 위해 클래스 도입
+// 1. 드라이버의 정보(FD)를 보존하기 위해 클래스 도입
 // 2. 둘 이상의 객체를 생성하는 것은 의미가 없으므로 싱글톤 패턴 도입
 class ProcessState
 {
@@ -28,7 +28,7 @@ public:
 };
 ProcessState *ProcessState::pInstance = 0;
 
-// 3. 드라이버와 통신을 하기 위한 클래스를 도입
+// 3. 드라이버와 통신(binder_call)을 하기 위한 클래스를 도입
 // 4. 둘 이상의 객체를 생성하는 것은 의미가 없으므로 싱글톤 패턴 도입
 class IPCThreadState
 {
@@ -56,37 +56,38 @@ public:
 };
 IPCThreadState  *IPCThreadState::iInstance = 0;
 
+// 5. 핸들을(Service manager 로부터 받은 service 의 handle) 보관하기 위한 클래스 도입 
+class BpBinder
+{
+	int mHandle;
+
+public:
+	BpBinder(int handle) : mHandle(handle) {}
+
+	int transact(int code, int *msg, int *reply)
+	{
+		return IPCThreadState::self()->transact(mHandle, code, msg, reply);
+	}
+};
 
 
 int main(int argc, const char *argv[])
 {
 	ProcessState *proc(ProcessState::self());
-	//IPCThreadState *ipc(new IPCThreadState);
-	//IPCThreadState *ipc(IPCThreadState::self());
 
 	int msg, reply;
 
-	// binder call 을 사용하지 않고 통신 클래스의 래핑 인터페이스를 사용
-	
-	//int handle = binder_call(proc->get_driver(), (int*)"LedService", &reply,
-	//		BINDER_SERVICE_MANAGER,	SVC_MGR_CHECK_SERVICE);
-
-
-	// ipc-> 대신 IPCThreadState::self()-> 의 문법으로 사용 (안드로이드 진영의 convension)
-
-	//int handle = ipc->transact(BINDER_SERVICE_MANAGER, SVC_MGR_CHECK_SERVICE,
-	//		(int*)"LedService",  &reply);
 	int handle = IPCThreadState::self()->transact(BINDER_SERVICE_MANAGER, SVC_MGR_CHECK_SERVICE,
 			(int*)"LedService",  &reply);
+	
+	BpBinder *binder = new BpBinder(handle);
 
 	while (1)
 	{
-		//getchar(); binder_call(proc->get_driver(), &msg, &reply, handle, 1);
-		//getchar(); ipc->transact(handle, 1, &msg, &reply);
-		getchar(); IPCThreadState::self()->transact(handle, 1, &msg, &reply);
-		//getchar(); binder_call(proc->get_driver(), &msg, &reply, handle, 2);
-		//getchar(); ipc->transact(handle, 2, &msg, &reply);
-		getchar(); IPCThreadState::self()->transact(handle, 2, &msg, &reply);
+		//getchar(); IPCThreadState::self()->transact(handle, 1, &msg, &reply);
+		getchar(); binder->transact(1, &msg, &reply);
+		//getchar(); IPCThreadState::self()->transact(handle, 2, &msg, &reply);
+		getchar(); binder->transact(2, &msg, &reply);
 	}
 
 	return 0;
